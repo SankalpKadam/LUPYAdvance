8b24db8881ee9655c9630defe42bfab2b28ec44c
SankalpKadam <67373254+SankalpKadam@users.noreply.github.com>
2020-10-19 12:19:44 +0530

    Update day13-exercise-ChangeFileExtension.py

--- Day13/day13-exercise-ChangeFileExtension.py
+++ Day13/day13-exercise-ChangeFileExtension.py
@@ -3,12 +3,12 @@ from configparser import ConfigParser
 config=ConfigParser()
 config.read("config-file.ini")
 dir=config.get("EXT","CurrentPath")
-from=config.get("EXT","from")
-to=config.get("EXT","to")
+fromthis=config.get("EXT","from")
+tothis=config.get("EXT","to")
 os.chdir(dir)
 for file in os.listdir("."):
-	if file.endswith(from):
+	if file.endswith(fromthis):
 		first_name=file.rsplit(".",1)[0]
-		new_name=first_name+to
+		new_name=first_name+tothis
 		print(file+" changed to -> "+new_name)
 		os.rename(file,new_name)
